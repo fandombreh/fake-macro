7c878036e6481ab0a673acc04e3da60be81314a3
fandombreh <168399187+fandombreh@users.noreply.github.com>
2025-04-04 09:58:35 -0600

    Update sigma sigma fake macro.lua

--- sigma sigma fake macro.lua
+++ sigma sigma fake macro.lua
@@ -1,45 +1,31 @@
--- Decompiler will be improved VERY SOON!
--- Decompiled with Konstant V2.1, a fast Luau decompiler made in Luau by plusgiant5 (https://discord.gg/wyButjTMhM)
--- Decompiled on 2025-04-04 08:31:36
--- Luau version 6, Types version 3
--- Time taken: 0.000931 seconds
+-local toggle = false
+local UIS = game:GetService("UserInputService")
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
+local LP = Players.LocalPlayer
 
-local var1_upvw = false
-local UserInputService = game:GetService("UserInputService")
-UserInputService.InputBegan:Connect(function(arg1, arg2) -- Line 7, Named "onInputBegan"
-    --[[ Upvalues[1]:
-        [1]: var1_upvw (read and write)
-    ]]
-    if arg2 then
-    elseif arg1.KeyCode == Enum.KeyCode.X then
-        var1_upvw = true
-    end
+UIS.InputBegan:Connect(function(input, gpe)
+	if not gpe and input.KeyCode == Enum.KeyCode.Q then
+		toggle = true
+	end
 end)
-UserInputService.InputEnded:Connect(function(arg1, arg2) -- Line 15, Named "onInputEnded"
-    --[[ Upvalues[1]:
-        [1]: var1_upvw (read and write)
-    ]]
-    if arg2 then
-    elseif arg1.KeyCode == Enum.KeyCode.X then
-        var1_upvw = false
-    end
+
+UIS.InputEnded:Connect(function(input, gpe)
+	if not gpe and input.KeyCode == Enum.KeyCode.Q then
+		toggle = false
+	end
 end)
-local Players_upvr = game:GetService("Players")
-game:GetService("RunService").Heartbeat:Connect(function() -- Line 26
-    --[[ Upvalues[2]:
-        [1]: Players_upvr (readonly)
-        [2]: var1_upvw (read and write)
-    ]]
-    local LocalPlayer = Players_upvr.LocalPlayer
-    if var1_upvw then
-        LocalPlayer.CameraMinZoomDistance = 0.5
-        LocalPlayer.CameraMode = Enum.CameraMode.LockFirstPerson
-        wait(0)
-        LocalPlayer.CameraMode = Enum.CameraMode.Classic
-        LocalPlayer.CameraMinZoomDistance = 5
-        wait(0)
-    else
-        LocalPlayer.CameraMinZoomDistance = 0.5
-        LocalPlayer.CameraMode = Enum.CameraMode.Classic
-    end
+
+RunService.Heartbeat:Connect(function()
+	if toggle then
+		LP.CameraMinZoomDistance = 0.5
+		LP.CameraMode = Enum.CameraMode.LockFirstPerson
+		task.defer(function()
+			LP.CameraMode = Enum.CameraMode.Classic
+			LP.CameraMinZoomDistance = 5
+		end)
+	else
+		LP.CameraMinZoomDistance = 0.5
+		LP.CameraMode = Enum.CameraMode.Classic
+	end
 end)
